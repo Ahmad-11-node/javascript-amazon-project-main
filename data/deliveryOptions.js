903c04e690f3fbc5e62e7ddb200be244165994c2
Ahmad Raza <mirzaahmad7000@gmail.com>
2025-10-09 14:44:49 +0500

    save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    deliveryDays: 7,
+    priceents: 0,
+  },
+  {
+    id: "2",
+    deliveryDays: 3,
+    priceents: 499,
+  },
+  {
+    id: "3",
+    deliveryDays: 1,
+    priceents: 999,
+  },
+];
